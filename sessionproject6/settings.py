440808b168a5c827cafae7f8739ad119d0ec9a79
Pragyandeep Mohanty <55882980+pragyandeep1@users.noreply.github.com>
2024-12-08 12:29:18 +0530

    Update settings.py

--- sessionproject6/settings.py
+++ sessionproject6/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = BASE_DIR/'static'
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config()
+SECRET_KEY = config(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
